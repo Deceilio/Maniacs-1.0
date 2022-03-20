7a8dbc394015e1787ee5a7c0fc709d9281aaaffe
Ravi Raj <rrrravi234@gmail.com>
2022-03-20 05:53:28 +0530

    Adding Light in scene

---
+++ Assets/Flooded_Grounds/Scripts/LHRotation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LHRotation : MonoBehaviour
+{
+    [SerializeField] GameObject lH_Lamp;
+    [SerializeField] float speed = 0.1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        lH_Lamp.transform.Rotate(0.0f,speed,0.0f,Space.World);
+        
+    }
+}
